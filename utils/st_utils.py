91194ff5f66e0feb3b73f238e7164d9f15e2db4e
Augusto Seabra <augusto.seabra00@gmail.com>
2025-06-09 15:38:34 -0300

    feat: streamlit utils

---
+++ utils/st_utils.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def setup_sidebar():
+  st.sidebar.markdown('## Referências principais')
+  st.sidebar.markdown('[Clash Of Clans Wiki (Fandom)](https://clashofclans.fandom.com/wiki/Clash_of_Clans_Wiki)')
+  st.sidebar.markdown('[Clash of Clans (Supercell)](https://supercell.com/en/games/clashofclans/pt/blog/)')
+  st.sidebar.markdown('## Desenvolvimento')
+  st.sidebar.markdown('Desenvolvido por [Augusto Seabra](https://github.com/skti-dev)')
+  st.sidebar.markdown('Código fonte disponível em [GitHub](https://github.com/skti-dev/coc-insights)')
